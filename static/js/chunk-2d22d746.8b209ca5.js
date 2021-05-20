ea3a94a2db5f06f64a6c8c5d2f92d0bd61c40edf
LJH95224 <991538766@qq.com>
2021-05-20 02:03:09 +0000

    Deploying to gh-pages from @ LJH95224/manager-admin@79bd9f2e854dbf4a5ebc48068f419abeb2a984a8 🚀

---
+++ static/js/chunk-2d22d746.8b209ca5.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(n,t,c){"use strict";c.r(t);var e=c("7a23"),a={class:"about"},s=Object(e["n"])("h1",null,"This is an about page",-1);function u(n,t){return Object(e["E"])(),Object(e["i"])("div",a,[s])}const i={};i.render=u;t["default"]=i}}]);

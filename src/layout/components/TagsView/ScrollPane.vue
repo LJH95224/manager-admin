a1dba4cc1ec4f32cfffc90a04bb62087e33ee1d7
LJH95224 <991538766@qq.com>
2021-05-26 18:31:12 +0800

    'tagview'

---
+++ src/layout/components/TagsView/ScrollPane.vue
@@ -0,0 +1,26 @@
+<!--
+ * @Description: tagview 滚动条
+ * @Autor: Alfred
+ * @Date: 2021-05-26 14:22:21
+ * @LastEditTime: 2021-05-26 14:22:36
+ * @FilePath: \manager-admin\src\layout\components\TagsView\ScrollPane.vue
+-->
+<template>
+  <el-scrollbar>
+    <slot />
+  </el-scrollbar>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
--- src/layout/components/TagsView/index.vue
+++ src/layout/components/TagsView/index.vue
@@ -2,6 +2,45 @@
  * @Description: 标签视图组件
  * @Autor: Alfred
  * @Date: 2021-05-18 09:01:04
- * @LastEditTime: 2021-05-18 09:01:05
- * @FilePath: \backManager\src\layout\components\TagsView\index.vue
+ * @LastEditTime: 2021-05-26 16:56:58
+ * @FilePath: \manager-admin\src\layout\components\TagsView\index.vue
 -->
+<template>
+  <scroll-pane class="tags-view-scroll">
+    <router-link
+      v-for="tag in visitedViews"
+      ref='tag'
+      :key='tag.path'
+      :to="{path: tag.path, query: tag.query, fullPath: tag.fullPath}"
+      tag='span'
+      class="tags-view-item">
+      {{ tag.title.title }}
+      <span class="el-icon-close"></span>
+    </router-link>
+  </scroll-pane>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from 'vue'
+import ScrollPane from './ScrollPane.vue'
+import { RouteLocationNormalized } from 'vue-router'
+
+export interface TagView extends Partial<RouteLocationNormalized> {
+  title?: string
+}
+export default defineComponent({
+  components: {
+    ScrollPane
+  },
+  setup() {
+    const state = reactive({
+      visitedViews
+    })
+    return {}
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
